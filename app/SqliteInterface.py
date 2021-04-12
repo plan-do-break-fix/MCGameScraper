ed66e327c5ecaaff7c7cc47c81616c00db0da5d5
plan-do-break-fix <jeffdswan@outlook.com>
2021-04-12 18:39:47 -0500

    feature - no longer returns first incomplete crawl, pull first twenty and returns random.choice

--- app/SqliteInterface.py
+++ app/SqliteInterface.py
@@ -1,4 +1,6 @@
-import os, sqlite3
+import os
+from random import choice
+import sqlite3
 
 
 PLATFORMS = [
@@ -32,7 +34,8 @@ GENRES = [
 SCHEMAS = {
     "platforms": (
         "CREATE TABLE IF NOT EXISTS 'platforms' ("
-        "  slug TEXT NOT NULL"
+        "  slug TEXT NOT NULL,"
+        "  genre_crawl_complete INTEGER DEFAULT 0"
         ");"
     ),
     "games": (
@@ -74,7 +77,8 @@ SCHEMAS = {
     "platform_genre_crawls": (
         "CREATE TABLE IF NOT EXISTS 'platform_genre_crawls' ("
         "  platform INTEGER NOT NULL,"
-        "  url STRING NOT NULL,"
+        "  genre STRING NOT NULL,"
+        "  url STRING DEFAULT NULL,"
         "  final_page_number INTEGER DEFAULT NULL,"
         "  last_page_scraped INTEGER DEFAULT NULL,"
         "  FOREIGN KEY (platform) REFERENCES platforms (rowid)"
@@ -110,46 +114,86 @@ class Interface:
 
     def get_platform_slug(self, platform_pk) -> str:
         self.c.execute("SELECT slug FROM platforms WHERE rowid=?", (platform_pk,))
-        return self.c.fetchone()
+        return self.c.fetchone()[0]
 
     def get_platform_pk_from_genre_crawl_url(self, url):
         self.c.execute("SELECT platform FROM platform_genre_crawls WHERE url=?",
                        (url))
-        return self.c.fetchone()
+        return self.c.fetchone()[0]
+
+    def get_platforms_without_genre_crawl_urls(self):
+        self.c.execute("SELECT slug FROM platforms WHERE genre_crawl_complete=0")
+        return self.c.fetchall()
+
+    def get_remaining_title_crawls(self):
+        self.c.execute("SELECT url, last_page_scraped, final_page_number FROM platform_genre_crawls")
+        crawls = self.c.fetchall()
+        return [_c for _c in crawls if _c[1] != _c[2]]
+
+    def get_one_incomplete_game_crawl(self):
+        self.c.execute("SELECT * FROM games "
+                       "WHERE final_user_review_page_number=NULL"
+                       "OR final_critic_review_page_number=NULL "
+                       "LIMIT=20")
+        result = self.c.fetchall()
+        if not result:
+            self.c.execute("SELECT * FROM games "
+                           "WHERE last_user_review_page_scraped!=final_user_review_page_number"
+                           "OR last_critic_review_page_scraped!=final_critic_review_page_number "
+                           "LIMIT=20")
+            result = self.c.fetchall()
+            if not result:
+                return False
+        result = choice(result)
+        if result[5] > result[6]:
+            page = 0 if not result[6] else result[6] + 1
+            crawl = []
+            crawl.append("user")
+        elif result[7] > result[8]:
+            page = 0 if not result[8] else result[8] + 1
+            crawl = []
+            crawl.append("critic")
+        
+
+
+
+
 
     # Existence checks - return rowid if exists else False
     def platform_exists(self, slug: str):
         self.execute("SELECT rowid FROM platforms WHERE slug=?", (slug,))
         result = self.c.fetchone()
-        return result if result else False
+        return result[0] if result else False
 
     def game_exists(self, slug, platform):
         platform_pk = self.platform_exists(platform)
         self.c.execute("SELECT rowid FROM games WHERE slug=? AND platform=?",
                        (slug, platform_pk))
         result = self.c.fetchone()
-        return result if result else False
+        return result[0] if result else False
 
     def user_review_exists(self, review_id: str):
         self.c.execute("SELECT rowid FROM user_reviews WHERE review_id=?",
                        (review_id,))
         result = self.c.fetchone()
-        return result if result else False
+        return result[0] if result else False
 
     def critic_review_exists(self, author, date):
         self.c.execute("SELECT rowid FROM critic_reviews WHERE author=? AND date=?",
                        (author, date))
         result = self.c.fetchone()
-        return result if result else False
+        return result[0] if result else False
 
     def platform_genre_crawl_url_exists(self, url):
         self.c.execute("SELECT rowid FROM platform_genre_crawls WHERE url=?",
                        (url,))    
         result = self.c.fetchone()
-        return result if result else False
+        return result[0] if result else False
     
     # Row insertion
     def new_game(self, title, slug, platform_slug, released) -> bool:
+        if self.game_exists(slug, platform_slug):
+            return False
         platform_pk = self.platform_exists(platform_slug)
         self.c.execute("INSERT INTO games (title, slug, platform, released) \
                        VALUES (?,?,?,?)",
@@ -158,6 +202,8 @@ class Interface:
         return self.c.lastrowid
 
     def new_critic_review(self, game_pk, review_id, author, date, grade, body):
+        if self.critic_review_exists(author, date):
+            return False
         game_pk = self.game_exists(game_slug)
         self.c.execute("INSERT INTO critic_reviews "
                        "(game, review_id, author, date, grade, body) "
@@ -167,11 +213,20 @@ class Interface:
 
     def new_user_review(self, game_slug, review_id, author, date, grade, body,
                               votes_total, votes_helpful):
+        if self.user_review_exists(review_id):
+            return False
         game_pk = self.game_exists(game_slug)
         self.c.execute("INSERT INTO critic_reviews "
                        "(game, review_id, author, date, grade, body) "
                        "VALUES (?,?,?,?,?,?)",
                        (game_pk, review_id, author, date, grade, body))
+        self.conn.commit()
+
+    def new_platform_genre_crawl_url(self, platform_pk, url: str):
+        if self.platform_genre_crawl_url_exists(url):
+            return False
+        self.c.execute("INSERT INTO platform_genre_crawls (platform, url) "\
+                       "VALUES (?,?)", (platform_pk, url,))
         self.conn.commit()
 
     # Progress tracking methods - final_page_number and last_page_scraped
@@ -189,12 +244,6 @@ class Interface:
         self.c.execute(f"UPDATE games "
                         "SET last_{review_type}_review_page_scraped=? "
                         "WHERE rowid=?", (number))
-        self.conn.commit()
-
-    # Progress tracking methods - platform genre crawl
-    def new_platform_genre_crawl_url(self, platform_pk, url: str):
-        self.c.execute("INSERT INTO platform_genre_crawls (platform, url) "\
-                       "VALUES (?,?)", (platform_pk, url,))
         self.conn.commit()
 
     def add_final_platform_genre_page_number(self, url, number):
