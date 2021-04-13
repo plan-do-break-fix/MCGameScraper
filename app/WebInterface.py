af80848bbdd15b66fdb8a9e0f5a29a5e166ce1fe
plan-do-break-fix <jeffdswan@outlook.com>
2021-04-12 21:38:28 -0500

    feature - logging

--- app/WebInterface.py
+++ app/WebInterface.py
@@ -15,24 +15,28 @@ HEADERS = [
 
 class Interface:
 
-    def __init__(self, throttle_seconds=10):
+    def __init__(self, logger, throttle_seconds=10):
+        self.log = logger
         self.throttle_seconds = throttle_seconds
         self.last_fetch = 0
 
     def fetch(self, url: str) -> str:
         """Return the HTML contained in HTTP response."""
         self.throttle()
+        self.log.debug(f"Attempting to fetch {url}.")
         resp = requests.get(url, headers=HEADERS[0])
         self.update_last_fetch()
+        self.log.debug(f"HTTP status code {resp.status_code}")
         if not resp.status_code == 200:
             return None
         return resp.content.decode()
 
     def throttle(self) -> None:
         """Restrict HTTP GETs to no more than once per throttle_seconds."""
+        self.log.debug("Throttling...")
         delta = int(datetime.now().timestamp()) - self.last_fetch
         if delta < self.throttle_seconds:
-            sleep(int(self.last_fetch - delta))
+            sleep(int(self.throttle_seconds - delta))
 
     def update_last_fetch(self) -> None:
         self.last_fetch = int(datetime.now().timestamp())
